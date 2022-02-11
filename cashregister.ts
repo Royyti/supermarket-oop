76b3ba022074ea0fc47cc1b3907d5770b01f334f
Royyti <93621111+Royyti@users.noreply.github.com>
2022-02-11 13:13:33 +0200

    Create cashregister.ts

---
+++ cashregister.ts
@@ -0,0 +1,15 @@
+import { Products } from "./products";
+import { StaffMember } from "./staffmember"
+export class CashRegister {
+    private static cashIdCounter: number = 1;
+    cashID: number;
+    products: Products[];
+    worker: StaffMember;
+
+    constructor(worker: StaffMember) {
+        this.cashID = CashRegister.cashIdCounter;
+        CashRegister.cashIdCounter++
+        this.products = [];
+        this.worker = worker;
+    }
+}
